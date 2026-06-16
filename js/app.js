41e063b6229d4f13e3343c18ada28ed55a1131d2
Pavel Noe Maqueda Gonzalez <noemaqueda10@gmail.com>
2026-06-16 08:50:12 -0600

    Subir proyecto Portal Equipo 5

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function cambiarColor(){
+
+let encabezado =
+document.querySelector("header");
+
+if(
+encabezado.style.background
+=="rgb(45, 140, 255)"
+){
+
+encabezado.style.background=
+"#091322";
+
+}
+
+else{
+
+encabezado.style.background=
+"#2d8cff";
+
+}
+
+}
